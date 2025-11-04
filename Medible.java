75f511bf7baf3437b9af7c4df1ac3a31d3b4e38d
nat <nataliavaliente2016@gmail.com>
2025-11-03 22:54:25 -0600

    Medible.java

---
+++ Medible.java
@@ -0,0 +1,11 @@
+interface Medible {
+    String obtenerMedicion();
+}
+
+interface Accionable {
+    void ejecutarAccion(String accion);
+}
+
+interface Registrable {
+    void registrarDatos(String datos);
+}
